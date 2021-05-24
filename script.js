9fa004e79942e8f7967a5534ffdcb5179bdc5f7a
Gokul Bijoy <81373116+carlmax-here@users.noreply.github.com>
2021-05-24 12:16:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Blogger", "Bot Dev", "Student", "Fast Learner"],
+        strings: ["Python Noob", "Bot Dev", "Student", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Blogger", "Bot Dev", "Student", "Fast Learner"],
+        strings: ["Python Noob", "Bot Dev", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
